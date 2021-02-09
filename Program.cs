f17f5f546c653a2e3b9f6425c27fb3bc444b85d3
Anarki2600 <anarki26@gmail.com>
2021-02-09 23:43:15 +0800

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace dz6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Process[] processlist = Process.GetProcesses();
+
+            foreach (Process theprocess in processlist)
+            {
+                Console.WriteLine("Process: {0} ID: {1}", theprocess.ProcessName, theprocess.Id);
+            }
+            
+            two:
+            Console.WriteLine("Введите ID процесса для завершения");
+            string id = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(id))
+                goto two;
+            else
+            {
+                goto one;
+            }
+            one:
+
+            Process programma = Process.GetProcessById(Int32.Parse(id));
+            programma.Kill();
+            programma.WaitForExit();
+
+            Process[] processlist2 = Process.GetProcesses();
+
+            foreach (Process theprocess in processlist2)
+            {
+                Console.WriteLine("Process: {0} ID: {1}", theprocess.ProcessName, theprocess.Id);
+            }
+            if (programma.Responding)
+            {
+                Console.WriteLine("Status = Running");
+            }
+            else
+            {
+                Console.WriteLine("Status = Not Responding");
+            }
+
+
+
+
+
+            Console.ReadKey();
+            
+        }
+        
+    }
+}
